2f71dbdee88a048d3c1cd524a0ce19b067501aaf
anuj singh <anujs1430>
2024-03-17 20:16:13 +0530

    added a new file

---
+++ src/Sample.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sample = () => {
+  return (
+    <div>
+      this is the sample file to check git hub functionality
+    </div>
+  )
+}
+
+export default Sample
